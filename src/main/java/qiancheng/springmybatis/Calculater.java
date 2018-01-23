e2f43ce3ac5f6e95a36159ba948f914d397480a4
a756823019 <756823019@qq.com>
2018-01-23 16:50:12 +0800

    init

---
+++ src/main/java/qiancheng/springmybatis/Calculater.java
@@ -0,0 +1,10 @@
+package qiancheng.springmybatis;
+
+public class Calculater {
+	public double add(double a,double b) {
+		return a+b;
+	}
+	public double sub(double a,double b) {
+		return a-b;
+	}
+}
